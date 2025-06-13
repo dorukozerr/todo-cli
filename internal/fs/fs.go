cea763dbc62b1b8cd78f4e40f48a20164037a52a
Doruk Özer <dorukozer@protonmail.com>
2025-06-13 04:48:59 +0300

    refactor: fs file cleanup

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -2,12 +2,10 @@ package fs
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/dorukozerr/todo-cli/internal/types"
-	"github.com/dorukozerr/todo-cli/internal/utils"
 )
 
 func GetConfig() (*types.Config, error) {
@@ -79,97 +77,4 @@ func SaveConfig(config *types.Config) error {
 	}
 
 	return os.WriteFile(configPath, configData, 0644)
-}
-
-func CreateGroup(name string) error {
-	config, err := GetConfig()
-	if err != nil {
-		return err
-	}
-
-	for _, group := range config.Groups {
-		if group.Name == name {
-			return fmt.Errorf("group '%s' already exists", name)
-		}
-	}
-
-	id, err := utils.GenerateRandomID()
-	if err != nil {
-		return err
-	}
-
-	newGroup := types.Group{
-		ID:   id,
-		Name: name,
-	}
-
-	config.Groups = append(config.Groups, newGroup)
-	config.ActiveGroup = newGroup.Name
-
-	return SaveConfig(config)
-}
-
-func DeleteGroup(groupID string) error {
-	config, err := GetConfig()
-	if err != nil {
-		return err
-	}
-
-	var newGroups []types.Group
-	var deletedGroupName string
-
-	for _, group := range config.Groups {
-		if group.ID != groupID {
-			newGroups = append(newGroups, group)
-		} else {
-			deletedGroupName = group.Name
-		}
-	}
-
-	if deletedGroupName == "" {
-		return fmt.Errorf("group not found")
-	}
-
-	config.Groups = newGroups
-
-	var newTodos []types.Todo
-
-	for _, todo := range config.Todos {
-		if todo.Group != deletedGroupName {
-			newTodos = append(newTodos, todo)
-		}
-	}
-
-	config.Todos = newTodos
-
-	if config.ActiveGroup == deletedGroupName {
-		config.ActiveGroup = ""
-	}
-
-	return SaveConfig(config)
-}
-
-func SetActiveGroup(groupName string) error {
-	config, err := GetConfig()
-	if err != nil {
-		return err
-	}
-
-	found := false
-
-	for _, group := range config.Groups {
-		if group.Name == groupName {
-			found = true
-
-			break
-		}
-	}
-
-	if !found {
-		return fmt.Errorf("group '%s' does not exist", groupName)
-	}
-
-	config.ActiveGroup = groupName
-
-	return SaveConfig(config)
 }
